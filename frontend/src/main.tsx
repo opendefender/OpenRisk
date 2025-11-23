1d6e0532b23de8b49b7c94f41011320172403f9f
alex-dembele <alexandredembele16@gmail.com>
2025-11-23 09:40:41 +0100

    Toast position uodate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+import 'leaflet/dist/leaflet.css';
 import './index.css'
 import { Toaster } from 'sonner' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster position="bottom-right" theme="dark" richColors />
+    <Toaster position="top-right" theme="dark" richColors closeButton />
   </React.StrictMode>,
 )
