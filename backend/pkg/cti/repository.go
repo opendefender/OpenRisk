87682d04c300ee3bd22ef179da86314379a5f131
alex-dembele <alexandredembele16@gmail.com>
2026-06-26 11:54:28 +0200

    feat(cti): implement GORM CTI repository with MatchByAssetCPEs and batched upserts

---
+++ backend/pkg/cti/repository.go
@@ -0,0 +1,8 @@
+package cti
+
+// Repository interface is defined in cti.go alongside the Service interface.
+// This file previously contained the Repository interface definition.
+// The interface has been moved to cti.go for cohesion.
+//
+// The GORM implementation lives in:
+//   internal/infrastructure/repository/gorm_cti_repository.go
